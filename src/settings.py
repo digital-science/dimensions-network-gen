05a00cbab006bb13d07527f912ce4a6fed893964
lambdamusic <michele.pasin@gmail.com>
2022-08-05 18:12:19 +0100

    improve naming

--- src/settings.py
+++ src/settings.py
@@ -12,18 +12,17 @@ import os
 HERE = os.path.abspath(__file__)
 PROJECT_ROOT = os.path.dirname(os.path.dirname(HERE))  # one level above 'networkgen' folder
 
-PROJECT_STATIC_FOLDER =  PROJECT_ROOT + "/src/static" 
-DEFAULT_OUTPUT_LOCATION = PROJECT_ROOT + "/user-outputs"
-DEFAULT_OUTPUT_NETWORKS = DEFAULT_OUTPUT_LOCATION + "/networks"
+PROJECT_STATIC_PATH =  PROJECT_ROOT + "/src/static" 
+DEFAULT_OUTPUT_PATH = PROJECT_ROOT + "/build"
+DEFAULT_OUTPUT_JSON_PATH = DEFAULT_OUTPUT_PATH + "/json"
+
 DEFAULT_NETWORK_INIT = PROJECT_ROOT + "/src/networkgen/config_default.ini"
 
 
 # network visualizations tasks available
-
 NETWORK_TYPES = ['collab_orgs', 'concepts']
 
 # network visualizations parameters
-
 NETWORK_PARAMETERS_DEFAULT = {
     'network_types' : ", ".join(NETWORK_TYPES), 
     'max_nodes' : 500, 
